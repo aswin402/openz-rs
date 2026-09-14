2ce180b253f55bd402eaebc4317df950b933b08f
aswin402 <aswinvishal402@gmail.com>
2026-09-15 02:00:20 +0530

    refactor(tools): extract embedded test suite into tools/sop_tests.rs

--- src/tools/sop.rs
+++ src/tools/sop.rs
@@ -56,18 +56,5 @@ impl Tool for TriggerSopTool {
 }
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[tokio::test]
-    async fn test_trigger_sop_tool_metadata() {
-        let config = Config::default();
-        let tool = TriggerSopTool { config };
-        assert_eq!(tool.name(), "trigger_sop");
-        assert!(tool.description().contains("Trigger a stateful"));
-
-        let params = tool.parameters();
-        assert_eq!(params["type"], "object");
-        assert!(params["properties"]["sop_id"].is_object());
-    }
-}
+#[path = "sop_tests.rs"]
+mod tests;
